b664032d5ca3024ab7909931234f4cc659fd3705
greghicks01 <greghicks@iinet.net.au>
2015-04-26 20:29:41 +1000

    initial commit

---
+++ REST Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Grapevine.Client;
+using Newtonsoft.Json;
+
+namespace REST_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RESTClient gc = new RESTClient("http://localhost:1234");
+            //gc.NetworkCredentials = new Grapevine.NetworkCredential("username", "password");
+            //var cookie = new Grapevine.Cookie("sessionid", "A113");
+            //gc.Cookies.Add(cookie);
+            var request = new RESTRequest("/user/{id}");
+            request.AddParameter("id", "1234");
+            request.Method = Grapevine.HttpMethod.GET;
+            request.ContentType = Grapevine.ContentType.JSON;
+            request.Payload = "{\"key\":\"value\"}";
+            request.ContentType = Grapevine.ContentType.JSON;
+            request.Payload = "{\"key\":\"value\"}";
+            var response = gc.Execute(request);
+        }
+    }
+}
